ec95a5a917299033a285eaf38d18f7d0dd872e20
Julian <julian.andreduerr@web.de>
2025-02-25 16:51:33 +0100

    added two more example songs

--- utils/audio-database.js
+++ utils/audio-database.js
@@ -9,4 +9,14 @@ export const audioDatabase = [
     cover: "./cover/ballin.jpg",
     path: "./audio/ballin.mp3",
   },
+  {
+    name: "JULI - Fireflies",
+    cover: "./cover/fireflies.jpg",
+    path: "./audio/fireflies.mp3",
+  },
+  {
+    name: "iNovation - Volkstempo",
+    cover: "./cover/volkstempo.jpg",
+    path: "./audio/volkstempo.mp3",
+  },
 ];
